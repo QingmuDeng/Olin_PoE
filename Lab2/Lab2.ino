6a7ac50abac11c6443a26465ae4316c7a1e3297f
QingmuDeng <qingmu.deng@students.olin.edu>
2018-09-18 23:30:12 -0400

    mvp scanner

--- Lab2/Lab2.ino
+++ Lab2/Lab2.ino
@@ -1,43 +1,67 @@
-
 #include <Servo.h>
 
-Servo servo1;
+Servo pan;
+uint8_t pan_angle=0;
+Servo tilt;
+uint8_t tilt_angle=60;
+const int sweep_increment = 4;
+const int sweep_delay = 75;
 
-const int analogInPin = A0;
-const int analogOutPin = 9;
-
+const int infrared = A0;
+const int measure_delay = 110; // 38.3+9.6+5.0 = 52.9
 int sensorVal = 0;
-int outputVal = 0;
-
-int potpin = 0;
-int val;
-
-
-
 
 void setup() {
   // put your setup code here, to run once:
-
-  Serial.begin(9600);
-  servo1.attach(10);
-
+  Serial.begin(115520);
+  pan.attach(10);
+  tilt.attach(9);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
-  sensorVal = analogRead(potpin);
-  outputVal = map(sensorVal, 0, 1023, 0, 180);
-  servo1.write(outputVal);
-  delay(15);
-
-  // analogWrite(analogOutPin, outputValue);
-
-  Serial.print("sensor = ");  Serial.print(sensorVal);  Serial.print(",");
-  Serial.print("output = ");  Serial.println(outputVal);
-
-  delay(400);
-
-  
-
+  pan_tilt_sweep();//
+  take_measure();
+  print_to_laptop();
+  Serial.println(sensorVal);
 }
+
+void pan_sweep(){
+  pan.write(pan_angle);
+  if(pan_angle > 180){
+    pan_angle = 0;
+  }
+}
+
+void tilt_sweep(){
+  tilt.write(tilt_angle);
+  tilt_angle += sweep_increment;
+  if(tilt_angle > 120){
+    tilt_angle = 60;
+    pan_angle += 2;//sweep_increment;
+  }
+}
+
+void pan_tilt_sweep(){
+  tilt_sweep();
+  pan_sweep();
+  delay(sweep_delay);
+}
+
+void take_measure(){
+  delay(measure_delay);
+  sensorVal = analogRead(A0);
+}
+
+void print_to_laptop(){
+  Serial.print("pan = ");  Serial.print(pan_angle);  Serial.print("    ");
+  Serial.print("tilt = ");  Serial.print(tilt_angle);  Serial.print("    ");
+  Serial.print("output = ");  Serial.println(sensorVal);
+}
+
+void serialEvent(){
+  //statements
+  
+}
+
+
--- Lab2/readServo.py
+++ Lab2/readServo.py
@@ -1,16 +1,39 @@
 import serial
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+import numpy as np
 
-arduinoComPort = "/dev/ttyACM0(Arduino/Genuino Uno)"
-
+arduinoComPort = "/dev/ttyACM0"
 # set baud rate
-baudRate = 9600
+baudRate = 115520
 
 # open serial port
 serialPort = serial.Serial(arduinoComPort, baudRate, timeout=1)
 
-while True:
-    lineOfData = serialPort.readline().decode()
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+i = 0
+lineOfData = ""
+
+while i < 300:
+    try:
+        lineOfData = serialPort.readline().decode()
+    except:
+        print("okay let's ignore it")
+
 
     if len(lineOfData) > 0:
+        val = [int(s) for s in lineOfData.split() if s.isdigit()]
+        if(len(val)<3):
+            continue
+        print(val)
 
-        sensor, outputval = (int(x) for x in lineOfData.split)
+        ax.scatter(val[2], val[0], val[1])
+        # plt.xlim([0, 200])
+        plt.xlabel('Pan')
+        # plt.ylim([0, 200])
+        plt.ylabel('Tilt')
+        i += 1
+
+plt.show()
+
