896d2f6edfc8f562beac21e2d4367a9c3dbb0dae
QingmuDeng <qingmu.deng@students.olin.edu>
2018-09-23 00:03:40 -0400

    code organization

--- Lab2/Lab2.ino
+++ Lab2/Lab2.ino
@@ -5,39 +5,50 @@ uint8_t pan_angle=60;
 Servo tilt;
 uint8_t tilt_angle=60;
 bool tilt_direction=true;
-const int sweep_increment = 4;
+const int sweep_increment = 2;
 const int sweep_delay = 75;
-
 const int infrared = A0;
-const int measure_delay = 110; // 38.3+9.6+5.0 = 52.9
+const int measure_delay = 265; // 5*(38.3+9.6+5.0) to reach a stable reading
 int sensorVal = 0;
+
+typedef enum{
+  Initialize,
+  Scanning,
+} State;
+volatile State state;
 
 void setup() {
   // put your setup code here, to run once:
+  state = Initialize;
   Serial.begin(115520);
-  pan.attach(9);
-  tilt.attach(10);
+  pan.attach(10);
+  tilt.attach(9);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  pan_tilt_sweep();//
-//  pan.write(0);
-//  tilt.write(97);
-  take_measure();
-  print_to_laptop();
-  Serial.println(sensorVal);
-}
-
-void pan_sweep(){
-  pan.write(pan_angle);
-  if(pan_angle > 90){
+  if(state == Initialize){
     pan_angle = 60;
+    tilt_angle=60;
+    pan.write(pan_angle);
+    tilt.write(tilt_angle);
+  }else if(state == Scanning){
+    pan_tilt_sweep();
+    take_measure();
+    print_to_laptop();
   }
 }
 
+void pan_sweep(){
+  /* Pan sweep definition */
+  if(pan_angle > 90){
+    pan_angle = 60;
+  }
+  pan.write(pan_angle);
+}
+
 void tilt_sweep(){
-  tilt.write(tilt_angle);
+  /* Tilt sweep definition */
   if(tilt_direction){
     tilt_angle += sweep_increment;
   }else{
@@ -45,20 +56,23 @@ void tilt_sweep(){
   }
   if(tilt_angle > 120){
     tilt_direction = false;
-    pan_angle += 2;//sweep_increment;
+    pan_angle += sweep_increment;
   }else if(tilt_angle<60){
     tilt_direction = true;
-    pan_angle += 2;//sweep_increment;
+    pan_angle += sweep_increment;
   }
+  tilt.write(tilt_angle);
 }
 
-void pan_tilt_sweep(){
+void pan_tilt_sweep(){ 
+  /* Sweep with delay to allow servo to move into position */
   tilt_sweep();
   pan_sweep();
   delay(sweep_delay);
 }
 
 void take_measure(){
+  /* Delay 5 times the measurment period before taking ADC reading */
   delay(measure_delay);
   sensorVal = analogRead(A0);
 }
@@ -66,12 +80,21 @@ void take_measure(){
 void print_to_laptop(){
   Serial.print("pan = ");  Serial.print(pan_angle);  Serial.print("    ");
   Serial.print("tilt = ");  Serial.print(tilt_angle);  Serial.print("    ");
-  Serial.print("output = ");  Serial.println(sensorVal);
+  Serial.print("output = ");  Serial.print(sensorVal); Serial.print("    "); Serial.println(state);
 }
 
 void serialEvent(){
   //statements
-  
+  while (Serial.available()) {
+    int a = Serial.read();
+    if(a == (int)'i'){
+      state = Scanning;
+      break;
+    }else if(a == (int)'e'){
+      state = Initialize;
+      break;
+    }
+  }
 }
 
 
--- Lab2/readServo.py
+++ Lab2/readServo.py
@@ -1,69 +1,126 @@
-import serial
+import serial, time
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 import numpy as np
 
-arduinoComPort = "/dev/ttyACM0"
-# set baud rate
-baudRate = 115520
+class Scanner():
+    def __init__(self):
+        self.fig = plt.figure()
+        self.ax = self.fig.add_subplot(111, projection='3d')
+        self.dataPointNum = 0
+        self.lineOfData = ""
+        self.measure = []
+        self.pan = []
+        self.tilt = []
+
+    def config_serial(self, baudrate=115520):
+        """ Set up the serial port to communicate with Arduino
+        """
+        try: # Try either ttyACM0 or ttyACM1 because Arduino switches around between these two
+            arduinoComPort = "/dev/ttyACM0"
+            # set baud rate
+            baudRate = baudrate
+            # open serial port
+            self.serialPort = serial.Serial(arduinoComPort, baudRate, timeout=1)
+            print('Serial Port ttyACM0')
+        except:
+            arduinoComPort = "/dev/ttyACM1"
+            # set baud rate
+            baudRate = baudrate
+            # open serial port
+            self.serialPort = serial.Serial(arduinoComPort, baudRate, timeout=1)
+            print('Serial Port ttyACM1\n')
+
+    def config_arduino(self):
+        """ Make Arduino ready for scanning
+        """
+        time.sleep(3) # Sleep to allow Arduino self reset
+        print("Resetting Servo positions")
+        self.serialPort.write(b'e') # "Initialize" state trigger event
+        time.sleep(3) # delay to allow servo to reset
+
+        print("Scanning is starting immediately.")
+        self.serialPort.write(b'i') # "Scanning" state trigger event
+
+    def read_data(self, dataNum=100):
+        """ Read data from UART
+        """
+        while self.dataPointNum < dataNum:
+            try:
+                self.lineOfData = self.serialPort.readline().decode()
+            except:
+                print("bad serial format")
+
+
+            if len(self.lineOfData) > 0:
+                print(self.lineOfData)
+                val = [int(s) for s in self.lineOfData.split() if s.isdigit()]
+                if(len(val)<3):
+                    continue
+                val[2] = interpolate(val[2])
+                if val[2] == 0:
+                    continue
+                print(val)
+
+                self.measure.append(val[2])
+                self.pan.append(val[0])
+                self.tilt.append(val[1])
+                
+                # plt.zlabel('Tilt')
+                self.dataPointNum += 1
+
+    def exit(self):
+        """ Store and plot the point cloud, reset Arduino
+        """
+        self.serialPort.write(b'e')
+        self.ax.set_xlabel('Distance')
+        self.ax.set_ylabel('Pan(degrees)')
+        self.ax.set_zlabel('Tilt(degrees)')
+        self.ax.scatter(self.measure, self.pan, self.tilt, color='b')
+        plt.show()
+
 
 def interpolate(sensorVal):
     distance = 0
     if sensorVal > 519:
-        print("Too close")
-    elif sensorVal <= 519 and sensorVal > 460:
-        distance = 65.01 - 0.085(sensorVal)
-    elif sensorVal <= 460 and sensorVal > 363:
-        distance = 60.278 - 0.076(sensorVal)
-    elif sensorVal <= 363 and sensorVal > 289:
-        distance = 78.47 - 0.122(sensorVal)
-    elif sensorVal <= 289 and sensorVal > 245:
-        distance = 93.23 - 0.173(sensorVal)
-    elif sensorVal <= 245 and sensorVal > 193:
-        distance = 110.637 - 0.244(sensorVal)
-    elif sensorVal <= 193 and sensorVal > 160:
-        distance = 152.631 - 0.462(sensorVal)
-    elif sensorVal <= 160 and sensorVal > 113:
-        distance = 182.502-0.649(sensorVal)
-    elif sensorVal <= 113 and sensorVal > 97:
-        distance = 234.79 - 1.111(sensorVal)
-    elif sensorVal <= 97 and sensorVal > 80:
-        distance = 271.6 - 1.49(sensorVal)
+        # print("Too close")
+        return 0
+    # elif sensorVal <= 519 and sensorVal > 460:
+    #     distance = 65.01 - 0.085*(sensorVal)
+    # elif sensorVal <= 460 and sensorVal > 363:
+    #     distance = 60.278 - 0.076*(sensorVal)
+    # elif sensorVal <= 363 and sensorVal > 289:
+    #     distance = 78.47 - 0.122*(sensorVal)
+    # elif sensorVal <= 289 and sensorVal > 245:
+    #     distance = 93.23 - 0.173*(sensorVal)
+    # elif sensorVal <= 245 and sensorVal > 193:
+    #     distance = 110.637 - 0.244*(sensorVal)
+    # elif sensorVal <= 193 and sensorVal > 160:
+    #     distance = 152.631 - 0.462*(sensorVal)
+    # elif sensorVal <= 160 and sensorVal > 113:
+    #     distance = 182.502-0.649*(sensorVal)
+    # elif sensorVal <= 113 and sensorVal > 97:
+    #     distance = 234.79 - 1.111*(sensorVal)
+    # elif sensorVal <= 97 and sensorVal > 80:
+    #     distance = 271.6 - 1.49*(sensorVal)
     elif sensorVal <= 80:
-        print("Too far away")
-    retrun distance
+        # print("Too far away")
+        return 0
+
+    return sensorVal
+
 
 def main():
-    # open serial port
-    serialPort = serial.Serial(arduinoComPort, baudRate, timeout=1)
 
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    i = 0
-    lineOfData = ""
+    scanner = Scanner()
 
-    while i < 300:
-        try:
-            lineOfData = serialPort.readline().decode()
-        except:
-            print("okay let's ignore it")
+    scanner.config_serial()
 
+    scanner.config_arduino() 
 
-        if len(lineOfData) > 0:
-            val = [int(s) for s in lineOfData.split() if s.isdigit()]
-            if(len(val)<3):
-                continue
-            val[2] = interpolate(val[2])
-            print(val)
+    scanner.read_data(10)
 
-            ax.scatter(val[2], val[0], val[1])
-            # plt.xlim([0, 200])
-            plt.xlabel('Pan')
-            # plt.ylim([0, 200])
-            plt.ylabel('Tilt')
-            i += 1
-
-    plt.show()
+    scanner.exit()
 
 if __name__ == "__main__":
     main()
