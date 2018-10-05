da3cf7d2ab5777854a070328f10b8e6be770762d
LSanten <leon.santen@icloud.com>
2018-10-05 10:12:08 -0400

    changed folder structure, code works

--- Lab3/serial_communication_test.ino.ino
+++ Lab3/serial_communication_test.ino.ino
@@ -9,6 +9,7 @@ Adafruit_MotorShield AFMS = Adafruit_MotorShield();
 Adafruit_DCMotor *leftMotor = AFMS.getMotor(3);
 Adafruit_DCMotor *rightMotor = AFMS.getMotor(4);
 
+int LED = 13;
 
 void setup() {
   // put your setup code here, to run once:
@@ -16,6 +17,7 @@ void setup() {
   
   AFMS.begin();
   Serial.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT);
 
 }
 
@@ -28,6 +30,8 @@ void loop() {
   //Read input
   int val = Serial.read() - '0';
 
+
+  //Press 1 for LED ON, MOTORS FORWARD
   if (val == 1)
   {
     leftMotor->setSpeed(150);
@@ -35,11 +39,16 @@ void loop() {
     rightMotor->setSpeed(150);
     rightMotor ->run(FORWARD);
     Serial.println("FORWARD");
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(500);
+    
   }
-  else
+  //Press 0 for LED off, MOTORS OFF
+  if (val == 0)
   {
     rightMotor->run(RELEASE);
     leftMotor->run(RELEASE);
     Serial.println("STOP");
+    digitalWrite(LED_BUILTIN, LOW);
   }
 }
