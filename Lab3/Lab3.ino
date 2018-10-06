b9723ca48d22f2a239ef021a84c3f50108cdc298
LSanten <leon.santen@icloud.com>
2018-10-06 14:35:26 -0400

    Drive forward when 1, stop when 0 key is pressed

--- Lab3/Lab3.ino
+++ Lab3/Lab3.ino
@@ -17,23 +17,56 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  leftMotor->setSpeed(150);
-  leftMotor ->run(FORWARD);
-  rightMotor->setSpeed(150);
-  rightMotor ->run(FORWARD);
-  delay(2000);
 
-  leftMotor->setSpeed(150);
-  leftMotor ->run(BACKWARD);
-  rightMotor->setSpeed(150);
-  rightMotor ->run(BACKWARD);
-  delay(2000);
+  //Have arduino wait for input
+  //while(Serial.available() == 0);
+  
 
-//  rightMotor->run(RELEASE);
-  leftMotor->run(RELEASE);
-  delay(1000);
+  //Read input
+  int val = Serial.read() - '0';
 
-  Serial.println("hi");
+  //Press 1 for LED ON, MOTORS FORWARD
+  if (val == 1)
+  {
+    leftMotor->setSpeed(150);
+    leftMotor ->run(FORWARD);
+    rightMotor->setSpeed(150);
+    rightMotor ->run(FORWARD);
+    Serial.println("FORWARD");
+    delay(500);
+    
+  }
+  //Press 0 for LED off, MOTORS OFF
+  if (val == 0)
+  {
+    rightMotor->run(RELEASE);
+    leftMotor->run(RELEASE);
+    Serial.println("STOP");
+  }
+
+
+
+
+
+
+// ANNA'S CODE   
+//  // put your main code here, to run repeatedly:
+//  leftMotor->setSpeed(150);
+//  leftMotor ->run(FORWARD);
+//  rightMotor->setSpeed(150);
+//  rightMotor ->run(FORWARD);
+//  delay(2000);
+//
+//  leftMotor->setSpeed(150);
+//  leftMotor ->run(BACKWARD);
+//  rightMotor->setSpeed(150);
+//  rightMotor ->run(BACKWARD);
+//  delay(2000);
+//
+////  rightMotor->run(RELEASE);
+//  leftMotor->run(RELEASE);
+//  delay(1000);
+//
+//  Serial.println("hi");
   
 }
